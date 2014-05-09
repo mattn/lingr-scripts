5248cea0b73eef2d283a7e4e16428ab2dc4c2a88
mattn <mattn.jp@gmail.com>
2014-05-10 02:16:23 +0900

    Add notify bots for Shougware

---
+++ neocomplete_issue.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use utf8;
+use 5.010000;
+use autodie;
+use Furl;
+use JSON;
+use GDBM_File;
+use Digest::SHA1 qw/sha1_hex/;
+use Encode;
+
+my $secret = shift;
+
+my $dup_path = '/home/mattn/tmp/dup-neocomplete-issue-lingr.db';
+
+my %dup;
+unless ($ENV{DEBUG}) {
+    tie %dup, 'GDBM_File', $dup_path, &GDBM_WRCREAT, 0640;
+}
+
+my $ua = Furl->new(agent => $0, timeout => 10);
+$ua->env_proxy;
+my $res = $ua->get('https://api.github.com/repos/Shougo/neocomplete.vim/issues');
+$res->is_success or die;
+my $dat = decode_json($res->content);
+for my $issue (@{$dat}) {
+    next if $dup{$issue->{id}}++;
+
+    my $msg = sprintf("%s\n%s",
+        $issue->{title},
+        $issue->{html_url});
+    print $msg;
+
+	my $res = $ua->post('http://lingr.com/api/room/say', [], [
+	    room => 'vim',
+	    bot  => 'vim_patch_notify_bot',
+	    bot_verifier => sha1_hex('vim_patch_notify_bot' . $secret),
+	    text => encode_utf8($msg),
+	]);
+}
